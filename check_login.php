b78407ac4fbfb9edcc59f31f2b6991db46d6247c
josebarrientosq <35469345+josebarrientosq@users.noreply.github.com>
2018-05-04 08:10:16 -0500

    Update check_login.php

--- check_login.php
+++ check_login.php
@@ -4,13 +4,17 @@ $con = new ConectorBD('localhost','general','1234');
 
 $respuesta['conexion']= $con->initConexion('agendaphp');
 if($respuesta['conexion']="OK"){
-	
-$consulta_usuario=$con->ejecutarQuery("SELECT * FROM usuarios where username=".$_POST['username']." AND password=".$_POST['password']);
-//	$consulta_usuario = $con->consultar(['usuarios'],['username'] , "WHERE username=".$_POST['username']."AND password=".$_POST['password']);
 
-//	if ($consulta_usuario!= null) 
- //    $respuesta['acceso'] = 'concedido';
-//    }else $respuesta['acceso'] = 'rechazado';
+
+$consulta_usuario=$con->ejecutarQuery("SELECT * FROM usuarios WHERE username='".$_POST['username']."' AND password ='".$_POST['password']."'");
+if($consulta_usuario->num_rows>0){
+	$respuesta['acceso'] = 'concedido';
+    }else $respuesta['acceso'] = 'rechazado';
+
+$respuesta["consulta"]=$consulta_usuario->fetch_array();
+$respuesta['a']=$_POST['username'];
+$respuesta['b']=$_POST['password'];
+
 } 
 
 echo json_encode($respuesta);
