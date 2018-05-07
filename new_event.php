d0c7f1fea54bdff42982d616aa7c2705b05642fd
josebarrientosq <35469345+josebarrientosq@users.noreply.github.com>
2018-05-06 23:30:15 -0500

    Update new_event.php

--- new_event.php
+++ new_event.php
@@ -1,29 +1,23 @@
 <?php
-  
+session_start(); 
 require('conector.php');
 $con = new ConectorBD('localhost','root','');
+//$test=1;
 
 $data['msg']= $con->initConexion('agendaphp');
 if($data['msg']="OK"){
 	
 	//$con->ejecutarQuery("INSERT INTO eventos (id,titulo,start_date,allDay,end_date,end_hour,start_hour) VALUES ('','demo2','2018-05-13','false','','','')");//ok
 
-$con->ejecutarQuery("INSERT INTO eventos (id,title,start_date,allDay,end_date,end_hour,start_hour) VALUES ('','".$_POST['titulo']."','".$_POST['start_date']."','".$_POST['allDay']."','".$_POST['end_date']."','".$_POST['end_hour']."','".$_POST['start_hour']."')" );
+//$con->ejecutarQuery("INSERT INTO eventos (id,title,start_date,allDay,end_date,end_hour,start_hour,iduser) VALUES ('','".$_POST['titulo']."','".$_POST['start_date']."','".$_POST['allDay']."','".$_POST['end_date']."','".$_POST['end_hour']."','".$_POST['start_hour']."','".$test."')");
 
-$consulta=$con->ejecutarQuery("SELECT title, start_date as 'start' FROM eventos");
+$con->ejecutarQuery("INSERT INTO eventos (id,title,start_date,allDay,end_date,end_hour,start_hour,iduser) VALUES ('','".$_POST['titulo']."','".$_POST['start_date']."','".$_POST['allDay']."','".$_POST['end_date']."','".$_POST['end_hour']."','".$_POST['start_hour']."','".$_SESSION['id']."')" ); //parcialmente ok
+
+
+$id=$con->ejecutarQuery("SELECT id FROM eventos ORDER BY id DESC LIMIT 1");
 }
 
-
-
-$data['msg']="OK";
-
-$dbdata = array();
-
-while ( $row = $consulta->fetch_assoc())  {
-	$dbdata[]=$row;
-  }
-
-$data['consulta']=$dbdata;
+$data['id']=$id->fetch_all();
 
 $con->cerrarConexion();
 
